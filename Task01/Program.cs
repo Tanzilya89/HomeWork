c161fa42b32ea662f30cb6d6e9c9250e5cd7f8a9
Tan <tilasovna@gmail.com>
2022-10-31 18:10:44 +0300

    Add Task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
